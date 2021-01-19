8af6ee5331aa3e42e90ee4a87184be75463626ef
문준혁 <dreamjh20@gmail.com>
2021-01-19 23:20:07 +0900

    baekjoon 2460

---
+++ 백준 2460/백준 2460/백준 2460.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int in(0), out(0), passenger(0), max(0);
+    for (int i = 0; i < 10; i++) {
+        cin >> out >> in;
+        passenger = passenger - out + in;
+        if (passenger > max) {
+            max = passenger;
+        }
+    }
+    cout << max;
+    return 0;
+    
+}

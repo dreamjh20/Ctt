738c1d341febb26d99a78a33d8559d8f097694b2
문준혁 <dreamjh20@gmail.com>
2021-01-19 23:20:59 +0900

    baekjoon 2908

---
+++ 백준 2908/백준 2908/백준 2908.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int swap(int num) {
+    int hund, ten, one;
+    one = num / 100;
+    ten = ((num % 100) / 10)*10;
+    hund = num % 10 *100;
+    return hund + ten + one;
+}
+
+int main()
+{
+    int num1, num2;
+    cin >> num1 >> num2;
+    /*cout << num1 / 100 << (num1 % 100) / 10 << num1 %10;*/
+    if (swap(num1) > swap(num2)) {
+        cout << swap(num1);
+    }
+    else
+    {
+        cout << swap(num2);
+    }
+}

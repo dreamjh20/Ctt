29ea2513eb0ac3447392170df7dad44a47ef8a7f
문준혁 <dreamjh20@gmail.com>
2021-01-21 22:54:25 +0900

    Debug Mode

---
+++ MuSeongT's Lesson/MuSeongT's Lesson/MuSeongT's Lesson.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+class Ctt
+{
+public:
+    Ctt() {
+        cout << "Ctt" << endl;
+    }
+    ~Ctt() {
+        cout << "~Ctt" << endl;
+    }
+};
+int main()
+{
+    cout << "START" << endl;
+    Ctt c;
+    cout << "FINISH" << endl;
+}

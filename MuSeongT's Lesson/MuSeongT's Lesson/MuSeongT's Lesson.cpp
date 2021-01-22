fecfe2d103ad910731a95a424d206a3b6879a728
문준혁 <dreamjh20@gmail.com>
2021-01-22 23:58:42 +0900

    Get&SetData

--- MuSeongT's Lesson/MuSeongT's Lesson/MuSeongT's Lesson.cpp
+++ MuSeongT's Lesson/MuSeongT's Lesson/MuSeongT's Lesson.cpp
@@ -4,16 +4,22 @@ using namespace std;
 class Ctt
 {
 public:
-    Ctt() {
-        cout << "Ctt" << endl;
-    }
-    ~Ctt() {
-        cout << "~Ctt" << endl;
-    }
+    Ctt() { cout << "Ctt" << endl; }
+    ~Ctt() { cout << "~Ctt" << endl; }
+
+    void SetData(int nParam) { m_nData = nParam;  }
+    int GetData() { return m_nData;  }
+/*private:*/
+    int m_nData;
 };
 int main()
 {
     cout << "START" << endl;
-    Ctt c;
+    Ctt a;
+    /*a.m_nData = 63;*/
+
+    a.SetData(20);
+    cout << a.GetData() << endl;
+    
     cout << "FINISH" << endl;
 }
